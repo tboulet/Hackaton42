db2b1472ff3e7461426fbdc661d0db3ba06ce9f8
anadege <anadege@student.42.fr>
2022-10-16 18:13:58 +0200

    ex08 done

--- ex08.py
+++ ex08.py
@@ -54,7 +54,7 @@ def salt_and_pepper(img):
 	for i in range(number_of_pixels):
 		y_coord=random.randint(0, row - 1)
 		x_coord=random.randint(0, col - 1)
-		img[0, y_coord, x_coord] = 1.7
+		img[0, y_coord, x_coord] = -1.7
 	img[img < 0.0] = 0.0
 	img[img > 1.0] = 1.7
 	return img
@@ -64,30 +64,6 @@ def find_number(img_part1, img_part2):
 	if np.var(img_part1) > np.var(img_part2):
 		return img_part1
 	return img_part2
-
-def deplace_bottom_img(empty_img, original, deplacement):
-	for row in range(empty_img.shape[1]):
-		if row >= deplacement:
-			empty_img[:, row, :] = original[:, row - deplacement, :]
-	return empty_img
-
-def deplace_upper_img(empty_img, original, deplacement):
-	for row in range(empty_img.shape[1]):
-		if row <= deplacement:
-			empty_img[:, row, :] = original[:, row + deplacement, :]
-	return empty_img
-
-def deplace_right_img(empty_img, original, deplacement):
-	for col in range(empty_img.shape[2]):
-		if col >= deplacement:
-			empty_img[:, :, col] = original[:, :, col - deplacement]
-	return empty_img
-
-def deplace_left_img(empty_img, original, deplacement):
-	for col in range(empty_img.shape[2]):
-		if col <= deplacement:
-			empty_img[:, :, col] = original[:, :, col + deplacement]
-	return empty_img
 
 dataset_name = 'datasets2/08_mnist_sum_noise_level'
 X_labeled = np.load(join(dataset_name, "X_labeled.npy"))
@@ -99,57 +75,38 @@ X_val = np.load(join(dataset_name, "X_val.npy"))
 y_labeled_adapted = y_labeled.copy()
 y_labeled_adapted[y_labeled == 8] = 1
 
-X_bottom = np.zeros((X_labeled.shape[0], 1, 28, 28))
-for i in range(X_labeled.shape[0]):
-	X_bottom[i] = deplace_bottom_img(X_bottom[i], X_labeled[i], 7)
-
-X_upper = np.zeros((X_labeled.shape[0], 1, 28, 28))
-for i in range(X_labeled.shape[0]):
-	X_upper[i] = deplace_bottom_img(X_upper[i], X_labeled[i], 7)
-
-X_left = np.zeros((X_labeled.shape[0], 1, 28, 28))
-for i in range(X_labeled.shape[0]):
-	X_left[i] = deplace_left_img(X_left[i], X_labeled[i], 7)
-
-X_right = np.zeros((X_labeled.shape[0], 1, 28, 28))
-for i in range(X_labeled.shape[0]):
-	X_right[i] = deplace_right_img(X_right[i], X_labeled[i], 7)
-
-X_labeled_concat = np.concatenate((X_labeled, X_bottom[:250], X_upper[250:500], X_left[500:750], X_right[750:1000]), axis=0)
-y_labeled_concat = np.concatenate((y_labeled_adapted, y_labeled_adapted[:250], y_labeled_adapted[250:500], y_labeled_adapted[500:750], y_labeled_adapted[750:1000]), axis=0)
 
 
-for img in range(X_labeled_concat.shape[0]):
-	if (y_labeled_concat[img]) == 1:
-		X_labeled_concat[img] = filter_img(X_labeled_concat[img])
-		#X_labeled_concat[img] = salt_and_pepper(X_labeled_concat[img])
+for img in range(X_labeled.shape[0]):
+	if (y_labeled[img]) == 1:
+		X_labeled[img] = filter_img(X_labeled[img])
 	else:
-		X_labeled_concat[img] = salt_and_pepper(X_labeled_concat[img])
-	#X_labeled_concat[img] = ndimage.gaussian_filter(X_labeled_concat[img], 2)
-	X_labeled_concat[img] = ndimage.median_filter(X_labeled_concat[img], 3)
+		X_labeled[img] = salt_and_pepper(X_labeled[img])
+	X_labeled[img] = ndimage.median_filter(X_labeled[img], 3)
+	X_labeled[img] = filter_img(X_labeled[img])
 
 for img in range(X_val.shape[0]):
 	X_val[img] = salt_and_pepper(X_val[img])
 	X_val[img] = filter_img(X_val[img])
+	X_val[img] = ndimage.median_filter(X_val[img], 3)
 
 for img in range(X_unlabeled.shape[0]):
 	X_unlabeled[img] = salt_and_pepper(X_unlabeled[img])
 	X_unlabeled[img] = filter_img(X_unlabeled[img])
+	X_unlabeled[img] = ndimage.median_filter(X_unlabeled[img], 3)
 
 # Check obtained images
 fig, axs = plt.subplots(10, 10, figsize=(50, 50))
 for i in range(10):
 	for j in range(10):
-		rd_ind = random.choice(range(X_labeled_concat.shape[0]))
-		axs[i, j].imshow(X_labeled_concat[rd_ind, 0], cmap='gray')
+		rd_ind = random.choice(range(X_labeled.shape[0]))
+		axs[i, j].imshow(X_labeled[rd_ind, 0], cmap='gray')
 		axs[i, j].axis('off')
-		axs[i, j].set_title(f"Data {rd_ind} label {y_labeled_concat[rd_ind]}", fontsize=7)
+		axs[i, j].set_title(f"Data {rd_ind} label {y_labeled[rd_ind]}", fontsize=7)
 fig.suptitle('Labeled data example')
 plt.show()
 
-exit()
-
-x_train, x_test, y_train, y_test = train_test_split(X_labeled_concat, y_labeled_concat)
+x_train, x_test, y_train, y_test = train_test_split(X_labeled, y_labeled_adapted)
 
 
 # Split labeled data into train and test
